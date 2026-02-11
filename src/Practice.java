f47300f0c716a27bce5b1be443d4f304ffc08466
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2026-02-11 17:30:59 -0500

    Update Practice.java

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,5 @@
 public class Practice {
-public static void main(String[] args) {
-    System.out.println("HELLO WORLD!");
-}
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
 }
