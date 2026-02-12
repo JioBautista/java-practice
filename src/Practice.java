5c91d754606bf593f96f68f3c910e0552889f470
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2026-02-12 12:29:24 -0500

    Update Practice.java
    Day 2 practice

--- src/Practice.java
+++ src/Practice.java
@@ -1,9 +1,13 @@
 public class Practice {
     public static void main(String[] args) {
+//        Non-primitive data types
         String firstName = "Jio";
         String lastName = "Pacho";
+//        Primitive data-types
         int age = 32;
-        final int employeeNum = 27574;
+        final int employeeNum = 0;
+        var companyName = "Rexall";
         System.out.println(firstName + " " + lastName + " " + age + " " + employeeNum);
+        System.out.println("Company: " + companyName);
     }
 }
