255f6b25e5ecd1da599eca571124618e76d2cfff
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2026-02-13 14:33:10 -0500

    Updates

--- src/Practice.java
+++ src/Practice.java
@@ -5,9 +5,16 @@ public class Practice {
         String lastName = "Pacho";
 //        Primitive data-types
         int age = 32;
-        final int employeeNum = 0;
+        final int employeeNum = 235002350;
         var companyName = "Rexall";
-        System.out.println(firstName + " " + lastName + " " + age + " " + employeeNum);
-        System.out.println("Company: " + companyName);
+        boolean isEmployee = true;
+
+        if (isEmployee) {
+            System.out.println("This person is an employee to " + companyName);
+            System.out.println(firstName + " " + lastName + " " + age + " " + employeeNum);
+            System.out.println("Company: " + companyName);
+        } else {
+            System.out.println("This person is NOT an employee to " + companyName);
+        }
     }
 }
