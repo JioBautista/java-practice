c46e778424b85ccdb63aa118c36dcb3e68a901b8
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2026-04-13 09:26:42 -0400

    Practice

--- src/Cashier.java
+++
@@ -1,7 +0,0 @@
-public class Cashier extends Employee {
-    int securityLevel = 5;
-
-    public static void main(String[] args) {
-        System.out.println("This is the cashier sub-class");
-    }
-}
